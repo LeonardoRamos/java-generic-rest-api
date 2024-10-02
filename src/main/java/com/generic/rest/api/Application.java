6edf0d3152738d761c07ef19b108e88ba008ece4
Leonardo Ramos <leoslramos@gmail.com>
2024-10-02 10:28:57 -0300

    Update properties

--- src/main/java/com/generic/rest/api/Application.java
+++ src/main/java/com/generic/rest/api/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaGenericRestApiApplication {
+public class Application {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(JavaGenericRestApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
